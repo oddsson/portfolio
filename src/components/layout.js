9adc8ab52d6328ee24ab95c80dd21e649adcd30d
Ívar Oddsson <ivaroddsson@gmail.com>
2020-08-11 15:45:07 +0000

    This is now mobile first

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react"
 import { createGlobalStyle } from "styled-components"
 import "typeface-archivo"
 import "typeface-caveat"
@@ -9,12 +9,28 @@ const GlobalStyle = createGlobalStyle`
     background-color: #ff793f;
   }
 
-  h1 {
+  h1, h2, h3 {
     font-family: "Archivo", sans-serif;
-    font-size: 6rem;
-    line-height: 8rem;
-    font-weight: 700;
     margin: 0;
+  }
+
+  h1 {
+    font-size: calc(30px + (42 - 30) * ((100vw - 320px) / (1600 - 320)));
+    font-weight: 700;
+    line-height: 1.5em;
+    margin: 0;
+
+    @media (min-width: 1440px) {
+      font-size: 6rem;
+    }
+  }
+
+  h2 {
+    font-size: 8em;
+
+    @media (min-width: 1440px) {
+      font-size: 15em;
+    }
   }
 `
 export default function Layout({ children }) {
