3298d6da639a14dac8ae3bf3ef6f63554a503224
Ívar Oddsson <ivaroddsson@gmail.com>
2020-08-12 16:07:44 +0000

    Added projects section

--- src/components/project.js
+++ src/components/project.js
@@ -2,35 +2,80 @@ import React from "react"
 import styled from "styled-components"
 
 const StyledProject = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
   border: 1px solid white;
-  padding: 1em;
+  padding: 10%;
   color: white;
+  min-height: 400px;
+
+  &:hover::after {
+    height: 100%;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: ${props => props.color};
+    z-index: -1;
+    width: 100%;
+    height: 0;
+    transition: height 0.8s cubic-bezier(0.25, 0.1, 0, 0.99);
+  }
 `
+
 const ProjectInfo = styled.span`
+  font-weight: 500;
   text-transform: uppercase;
   order: -1;
+  margin-bottom: 1em;
 `
 
 const ProjectTitle = styled.h3`
-  font-size: 2rem;
+  display: flex;
+  font-size: calc(30px + (60 - 30) * ((100vw - 320px) / (1440 - 320)));
+  margin-bottom: 1em;
 `
 
 const ProjectNumber = styled.span`
-  font-size: 0.9rem;
-  color: #ff793f;
-  text-shadow: -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff,
-    1px 1px 0 #fff;
+  font-size: 0.6rem;
+  margin: 6px 6px 0 0;
+  color: white;
   letter-spacing: 2px;
+
+  @media (min-width: 1440px) {
+    margin-top: 12px;
+  }
 `
 
-export default function Project({ name, skills, platform, projectNr }) {
+const ProjectAbout = styled.p`
+  margin: 0;
+`
+
+export default function Project({
+  name,
+  skills,
+  about,
+  platform,
+  projectNr,
+  color,
+}) {
   return (
-    <StyledProject>
+    <StyledProject color={color}>
       <ProjectTitle>
-        <ProjectNumber>{`0${projectNr}.`}</ProjectNumber>
+        <ProjectNumber>{`0${projectNr}`}</ProjectNumber>
         {name}
       </ProjectTitle>
-      <ProjectInfo>{`${platform} - ${skills}`}</ProjectInfo>
+      <ProjectInfo>{`${platform} - ${skills
+        .toString()
+        .replace(",", ", ")}`}</ProjectInfo>
+      <ProjectAbout>{about}</ProjectAbout>
+      <
     </StyledProject>
   )
 }
